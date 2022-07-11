27f1454fa4b9cf872039836beef4e904e7fda696
fukashigi1 <82066248+fukashigi1@users.noreply.github.com>
2022-07-10 23:14:42 -0400

    Main

--- index.py
+++ index.py
@@ -1,6 +1,28 @@
 class index:
-    print("Hola")
-
-
-#sexo
-#sexo2
+    def main(): 
+        print("Hola, Bienvenido a la interfaz, Porfavor seleccione una opción")
+        print("(1) Consultar Stock\n" +
+        "(2) Mostrar lista de productos para ver su caracteristicas\n"+
+        "(3) Acceder a la inter Agregarfaz de venta de productos \n"+
+        "(4) Historial de ventas de productos\n" +
+        "(5) Interfaz Agregar, eliminar y editar productos\n"+
+        "(6) Revisar ventas de procutos semanales")
+        option = int(input(""))
+        while option != 0:
+            if option == 1:
+                print 
+            elif option == 2:
+                print
+            elif option == 3:
+                print
+            elif option == 4:
+                print 
+            elif option == 5:
+                print
+            elif option == 0:
+                break
+            elif option == 6:
+                break
+            break
+    if __name__ == "__main__":
+        main()
