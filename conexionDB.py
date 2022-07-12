91b5e83953f7ae1f66732270b48618c9ab0889eb
fukashigi1 <82066248+fukashigi1@users.noreply.github.com>
2022-07-11 22:03:24 -0400

    conexion funcionando

--- conexionDB.py
+++ conexionDB.py
@@ -1,8 +1,16 @@
-#Aquí irá la conexión a phpMyAdmin
+import mysql.connector #En CMD poner "pip install mysql-connector-python"
 
-print("hola")
-print("chao")
-print("Paren de borrar mis weas")
-print("Si lees esto te quiero mucho")
-#COMENTARIO FUKA 1
-#hola
+#Tienen que tener la base de datos "pythontienda" que el fuka tienes que pasarles e implementarla en su phpmyadmin
+
+conn = mysql.connector.connect(host="localhost",port="3306",user="root",password="", database="pythontienda")
+cursor=conn.cursor()
+selectquery="select * from items"
+cursor.execute(selectquery)
+records = cursor.fetchall()
+print(cursor.rowcount)
+
+for row in records:
+    print(row[1])
+    
+cursor.close()
+conn.close()
